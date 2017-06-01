39aecdfd4844fc438d580ed590d41f414c5cccf7
Wei Xie <wei.xie@citrix.com>
2017-06-01 17:53:01 +0800

    Support to customize template name. Support debian-6 based OS. Support HVM based OS. Support Turbo, Linx, Yinhe.
    Signed-off-by: Wei Xie <wei.xie@citrix.com>

--- generator/gen.py
+++ generator/gen.py
@@ -7,13 +7,17 @@ def gen_uuid():
 
 class CustomOS:
     # parameter example: 'rhel', '6', # 'NeoKylin', 64
-    def __init__(self, baseOS, name, version, bit):
+    def __init__(self, baseOS, name, version, bit, templateName=''):
         self.baseOS = baseOS
         self.bit = bit
         self.version = version
         self.name = name
         self.reference_label = '{reference_name}-{version}-64bit'.format(
             reference_name=name.replace(' ', '-').lower(), version=version)
+        if templateName:
+            self.templateName = templateName
+        else:
+            self.templateName = name
 
     def genPattern(self):
         json = ''
@@ -28,9 +32,9 @@ class CustomOS:
 '''
             json = pattern.format(uuid=gen_uuid(),
                                   reference_label=self.reference_label,
-                                  version=self.version, name=self.name)
+                                  version=self.version, name=self.templateName)
 
-        if self.baseOS == 'rhel-5':
+        elif self.baseOS == 'rhel-5':
             pattern = '''\
 {{
    "uuid": "{uuid}",
@@ -41,7 +45,41 @@ class CustomOS:
 '''
             json = pattern.format(uuid=gen_uuid(),
                                   reference_label=self.reference_label,
-                                  version=self.version, name=self.name)
+                                  version=self.version, name=self.templateName)
+
+        elif self.baseOS == 'debian-6':
+            pattern = '''\
+{{
+    "uuid": "{uuid}",
+    "reference_label": "{reference_label}",
+    "name_label": "{name} {version} (64-bit)",
+    "derived_from": "base-debian-pv.json",
+    "min_memory": "512M",
+    "other_config": {{
+        "debian-release": "squeeze",
+        "install-arch": "amd64"
+    }}
+}}\
+'''
+            json = pattern.format(uuid=gen_uuid(),
+                                  reference_label=self.reference_label,
+                                  version=self.version, name=self.templateName)
+
+        elif self.baseOS == 'hvm':
+            pattern = '''\
+{{
+    "uuid": "{uuid}",
+    "reference_label": "{reference_label}",
+    "name_label": "{name} {version} (64-bit)",
+    "derived_from": "base-hvmlinux.json",
+    "min_memory": "512M",
+    "disks": [ {{ "size": "10G" }} ]
+}}\
+'''
+            json = pattern.format(uuid=gen_uuid(),
+                                  reference_label=self.reference_label,
+                                  version=self.version, name=self.templateName)
+
         return json
 
     def genJsonFile(self):
@@ -55,6 +93,14 @@ def main():
     myOS = CustomOS('rhel-6', 'Asianux Server', 4,  64)
     myOS.genJsonFile()
     myOS = CustomOS('rhel-5', 'Asianux Server', 3,  64)
+    myOS.genJsonFile()
+    myOS = CustomOS('rhel-6', 'Turbo', 12,  64, templateName='GreatTurbo Enterprise Server')
+    myOS.genJsonFile()
+    myOS = CustomOS('hvm', 'Linx', 8,  64, templateName='Linx Linux')
+    myOS.genJsonFile()
+    myOS = CustomOS('debian-6', 'Linx', 6,  64, templateName='Linx Linux')
+    myOS.genJsonFile()
+    myOS = CustomOS('hvm', 'Yinhe', 4,  64, templateName='Yinhe Kylin')
     myOS.genJsonFile()
 
 
